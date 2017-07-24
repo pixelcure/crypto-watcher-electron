5057feeb8c059e23c8fce20bd2791e91fe5bc3de
gadget <gadget@gadgets-MacBook-Pro.local>
2017-07-24 00:35:10 -0400

    adding header component and loading the Nav component in as a child. updating the index root

---
+++ client/src/components/Header/Header.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+
+it('renders without crashing', () => {
+  const header = document.createElement('header');
+  ReactDOM.render(<Header />, header);
+});
