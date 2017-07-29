dff163404f01f62f39059fe3d36ad88500aebf50
gadget <gadget@gadgets-MacBook-Pro.local>
2017-07-29 00:44:47 -0400

    updates to electron config, also updating styles for priceboard detail

--- client/src/components/Checkbox/index.js
+++ client/src/components/Checkbox/index.js
@@ -33,6 +33,7 @@ class Checkbox extends Component {
 			</fieldset>
 		);
 	}
+
 }
 
 
--- main.js
+++ main.js
@@ -12,9 +12,10 @@ function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
     transparent : true,
+    frameless : true,
     draggable : true,
-    width: 550,
-    height: 550
+    width: 275,
+    height: 790
   })
 
   // and load the index.html of the app.
